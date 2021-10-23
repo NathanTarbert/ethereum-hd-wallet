b0539af62b781657c3f6922d0a4d8bd895cf269e
Nathan Tarbert <nate4t@hotmail.comm>
2021-10-23 08:21:57 -0700

    add notes

--- 6.sign-transaction-hd-wallet.js
+++ 6.sign-transaction-hd-wallet.js
@@ -19,7 +19,7 @@ function deriveFiveWalletsFromNode(mnemonic, derivationPath) {
     return wallets;
 }
 
-let mnemonic = 'upset fuel enhance depart portion hope core animal innocent will athlete snack';
+let mnemonic = 'upset fuel enhance depart portion hope core animal innocent will athlete snack';// seed phrase
 let derivationPath = "m/44'/60'/0'/0";
 
 const wallets = deriveFiveWalletsFromNode(mnemonic, derivationPath);
@@ -32,7 +32,7 @@ async function signTransaction(wallet, toAdress, value) {
         // gasPrice: 1000000000,
         to: toAdress,// this is the address that you are sending ether to
         value: ethers.utils.parseEther(value), //value is the amount of ether that is sent. It must be passed in as a paramater or input box. As a number formatted as a string. ie "1.0"
-        // value: '0x16345785d8a0000',
+        // value: '0x16345785d8a0000', //example shown in Hex
         data: '0x',
     };
 
